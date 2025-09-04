c0a4a87d524ad4712b4c645d228d98bc2c8fd571
carol-xu4 <carol.xu@emory.edu>
2025-09-04 12:26:48 -0500

    aaaaaaa

--- data-code/mortality-code.r
+++ data-code/mortality-code.r
@@ -2,24 +2,15 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
 
-# Read in mortality data for 2020-2023 ------------------------------------
-# build file path
-for (y in 2020:2023) {
-    mort_path = paste0("data/input/VS", substr(y, 3, 4), "MORT.DUSMCPUB_r*.csv")
-
-# finding the file (release dates?)
-mort_year = Sys.glob(mort_path)
-
-# actually reading in the data
-mort_data = read_csv(mort_year)
-} 
-
-
 # Set working directory
-setwd("C:/Users/xucar/OneDrive/Desktop/mortality/data/input")
+setwd("C:/Users/xucar/OneDrive/Desktop/mortality")
 
-mort2017 = read_csv("mort2017.csv")
+# Read in mortality data for 2017-2020 ------------------------------------
+for (y in 2017:2020) {
+    mort.path = paste0("data/input/mort", y, ".csv")
+    mort.data = read.csv(mort.path, skip = 1)
+    }
+
+mort2017 = read.csv("data/input/mort2017.csv")
 print(colnames(mort2017))
-head(mort2017)
-
 
