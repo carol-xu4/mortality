c3425126b0e2b620319a24f9fed51cb150ff9701
carol-xu4 <carol.xu@emory.edu>
2025-09-05 17:14:05 -0500

    I DID IT

--- data-code/mortality-code.r
+++ data-code/mortality-code.r
@@ -12,13 +12,16 @@ setwd("C:/Users/xucar/OneDrive/Desktop/mortality")
         m1999 = read.csv("data/input/mort1999.csv")
         print(colnames(m1999))
 
-        m2007 = read.csv("data/input/mort2007.csv")
+        m1999 = read.csv("data/output/mort1999.csv.gz")
+        print(colnames(m1999))
+
+        m2007 = read.csv("data/output/mort2007.csv.gz")
         print(colnames(m2007))
 
 
 ## Read in mortality data for 2017-2020 ------------------------------------
 # useful variables:
-columns = c("educ", "sex", "age", "monthdth", "year", "race", "ucod", 
+columns = c("sex", "age", "monthdth", "year", "race", "ucod", 
     paste0("record_", 1:20))
 
 # Loop for output files containing useful variables only
@@ -30,12 +33,13 @@ for (y in 1999:2020) {
     write_csv(mort.data, paste0("data/output/mort", y, ".csv.gz"))
     }
 
-# final data [count = 11,918,140] 
+# final data [count = 56,911,051  ] 
 mort.path2 = paste0("data/output/mort", 1999:2020, ".csv.gz")
 mort = vroom::vroom(mort.path2,
     col_types = vroom::cols(.default = "c"),
     progress = TRUE
     )
+dim(mort)
 
 ## Descriptive stats  -----------------------------------------------------
 # underlying cause of death freq table
