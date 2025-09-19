be9eb0c0db6056b514fc5014fad36df9d5805eb3
carol-xu4 <carol.xu@emory.edu>
2025-09-19 17:15:33 -0500

    :(

--- analysis/analysis-code.r
+++ analysis/analysis-code.r
@@ -2,14 +2,23 @@
 
 ## Preliminaries -----------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, ggplot2, dplyr, knitr, ggthemes, lubridate, stringr, readxl, data.table, gdata, readr, arrow)
+pacman::p_load(tidyverse, ggplot2, dplyr, knitr, ggthemes, lubridate, stringr, data.table, gdata, readr, arrow)
 
 ## Set working directory
 setwd("C:/Users/xucar/OneDrive/Desktop/mortality")
 
-# filtering underlying cause of death = overdose
-data = arrow::open_dataset("data/output", format = "csv")
+# pulling output files using arrow
+records = paste0("record_", 1:20)
+columns = c("sex", "age", "monthdth", "year", "race", "ucod", records)
 
+strings = schema(!!!setNames(rep(list(utf8()), length(columns)), columns))
+
+data = arrow::open_dataset("data/output", format = "csv",
+    schema = strings, 
+    convert_options = csv_convert_options(null_values = c("", "NA"),
+    strings_can_be_null = TRUE))
+
+# filtering underlying cause of death = overdose
 overdose =  c("X40", "X41", "X42", "X43", "X44",
     "X60", "X61", "X62", "X63", "X64",
     "X85", 
@@ -18,7 +27,7 @@ overdose =  c("X40", "X41", "X42", "X43", "X44",
 od_counts = data %>%
     filter(ucod %in% overdose) %>%
     group_by(year) %>%
-    summarise(n = dplyr::n(), .groups = "drop") %>%
+    summarize(n = dplyr::n(), .groups = "drop") %>%
     collect() %>% 
     mutate(year = as.integer(year),
     n = as.numeric(n)) %>%
@@ -45,4 +54,32 @@ ggplot(od_counts, aes(x = year, y = n/1000)) +
 ggsave("results/overdose_deaths_by_year.png",
     width = 12, height = 8)
 
-# by substance type
+# by substance type & year
+icd_counts = data %>%
+    filter(ucod %in% overdose) %>%
+    select(year, all_of(records)) %>%
+    collect() %>%
+    mutate(
+        year = as.integer(year),
+        across(all_of(records), \(x) {
+            x = as.character(x)
+            x = trimws(x) # remove white space?
+            na_if(x, "") # replace empty strings with "NA"
+            })  
+    ) %>%
+    pivot_longer(cols = all_of(records),
+        names_to = "record",
+        values_to = "icd_code", 
+        values_drop_na = TRUE) %>%
+    group_by(year, icd_code) %>%
+    summarize(n = n(), .groups = "drop") %>%
+    arrange(year, desc(n))
+
+
+
+
+ 
+
+
+
+
