d4f9e0d790f97e8b2d04ba8f5e9a61171fc94016
carol-xu4 <carol.xu@emory.edu>
2025-09-19 15:21:13 -0500

    ugly plot :o

--- analysis/analysis-code.r
+++ analysis/analysis-code.r
@@ -2,7 +2,7 @@
 
 ## Preliminaries -----------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, ggplot2, dplyr, knitr, lubridate, stringr, readxl, data.table, gdata, readr, arrow)
+pacman::p_load(tidyverse, ggplot2, dplyr, knitr, ggthemes, lubridate, stringr, readxl, data.table, gdata, readr, arrow)
 
 ## Set working directory
 setwd("C:/Users/xucar/OneDrive/Desktop/mortality")
@@ -18,7 +18,30 @@ overdose =  c("X40", "X41", "X42", "X43", "X44",
 od_counts = data %>%
     filter(ucod %in% overdose) %>%
     group_by(year) %>%
-    summarise(n = dplyr::n(), .groups = "drop") 
+    summarise(n = dplyr::n(), .groups = "drop") %>%
+    collect() %>% 
+    mutate(year = as.integer(year),
+    n = as.numeric(n)) %>%
+    arrange(year)
 
-od_counts_results = knitr::kable(od_counts, format = "markdown")
-write_lines(od_counts_results, "results/od_counts")
+od_counts_table = knitr::kable(od_counts, format = "markdown") |> as.character()
+write_lines(od_counts_table, "results/od_counts")
+
+ggplot(od_counts, aes(x = year, y = n/1000)) +
+    geom_line(size = 2, color = "royalblue2") + 
+    labs (title = "Total Overdose Deaths by Year",
+    x = "Year", y = "Number of Deaths (in thousands)") +
+    scale_y_continuous(
+        limits = c(0, 100),
+        breaks = seq(0,100, 25)) +
+    theme_stata() + 
+    theme(
+        plot.title = element_text(size = 20, face = "bold"),
+        axis.title = element_text(size = 14, face = "bold"),
+        axis.text = element_text(size = 12),
+        axis.text.y = element_text(angle = 0),
+        plot.background = element_rect(fill = "white")
+    )
+ggsave("results/overdose_deaths_by_year.png",
+    width = 12, height = 8)
+
