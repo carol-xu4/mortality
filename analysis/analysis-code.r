4093e04a66949d593d0c81e06849b6321134ef33
carol-xu4 <carol.xu@emory.edu>
2025-09-19 22:06:04 -0500

    TABLES OWOWOWOW

--- analysis/analysis-code.r
+++ analysis/analysis-code.r
@@ -1,6 +1,6 @@
 ## ANALYSIS
 
-## Preliminaries -----------------------------------------------------------
+## Preliminaries --------------------------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, knitr, ggthemes, lubridate, stringr, data.table, gdata, readr, arrow)
 
@@ -18,7 +18,7 @@ data = arrow::open_dataset("data/output", format = "csv",
     convert_options = csv_convert_options(null_values = c("", "NA"),
     strings_can_be_null = TRUE))
 
-# filtering underlying cause of death = overdose ----------------------------
+# filtering underlying cause of death = overdose -------------------------------------------
 overdose =  c("X40", "X41", "X42", "X43", "X44",
     "X60", "X61", "X62", "X63", "X64",
     "X85", 
@@ -54,7 +54,7 @@ ggplot(od_counts, aes(x = year, y = n/1000)) +
 ggsave("results/overdose_deaths_by_year.png",
     width = 12, height = 8)
 
-# by substance type & year (INCLUDING DUPLICATES / POLY SUBSTANCE) -----------
+# by substance type & year (INCLUDING DUPLICATES / POLY SUBSTANCE) --------------------------
 icd_counts = data %>%
     filter(ucod %in% overdose) %>%
     select(year, all_of(records)) %>%
@@ -85,8 +85,67 @@ icd_counts = data %>%
 
 # top 20 icd codes in each year
 icd1999 = icd_counts %>% filter(year==1999) %>% arrange(desc(n)) %>% head(20)
-write_lines(knitr::kable(icd1999, format = "markdown"), "results/top_icd-10_tables/1999.md")
+    write_lines(knitr::kable(icd1999, format = "markdown"), "results/top_icd-10_tables/1999.md")
 
+icd2000 = icd_counts %>% filter(year==2000) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2000, format = "markdown"), "results/top_icd-10_tables/2000.md")
 
+icd2001 = icd_counts %>% filter(year==2001) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2001, format = "markdown"), "results/top_icd-10_tables/2001.md")
 
+icd2002 = icd_counts %>% filter(year==2002) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2002, format = "markdown"), "results/top_icd-10_tables/2002.md")
 
+icd2003 = icd_counts %>% filter(year==2003) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2003, format = "markdown"), "results/top_icd-10_tables/2003.md")
+
+icd2004 = icd_counts %>% filter(year==2004) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2004, format = "markdown"), "results/top_icd-10_tables/2004.md")
+
+icd2005 = icd_counts %>% filter(year==2005) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2005, format = "markdown"), "results/top_icd-10_tables/2005.md")
+
+icd2006 = icd_counts %>% filter(year==2006) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2006, format = "markdown"), "results/top_icd-10_tables/2006.md")
+
+icd2007 = icd_counts %>% filter(year==2007) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2007, format = "markdown"), "results/top_icd-10_tables/2007.md")
+
+icd2008 = icd_counts %>% filter(year==2008) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2008, format = "markdown"), "results/top_icd-10_tables/2008.md")
+
+icd2009 = icd_counts %>% filter(year==2009) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2009, format = "markdown"), "results/top_icd-10_tables/2009.md")
+
+icd2010 = icd_counts %>% filter(year==2010) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2010, format = "markdown"), "results/top_icd-10_tables/2010.md")
+
+icd2011 = icd_counts %>% filter(year==2011) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2011, format = "markdown"), "results/top_icd-10_tables/2011.md")
+
+icd2012 = icd_counts %>% filter(year==2012) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2012, format = "markdown"), "results/top_icd-10_tables/2012.md")
+
+icd2013 = icd_counts %>% filter(year==2013) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2013, format = "markdown"), "results/top_icd-10_tables/2013.md")
+
+icd2014 = icd_counts %>% filter(year==2014) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2014, format = "markdown"), "results/top_icd-10_tables/2014.md")
+
+icd2015 = icd_counts %>% filter(year==2015) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2015, format = "markdown"), "results/top_icd-10_tables/2015.md")
+
+icd2016 = icd_counts %>% filter(year==2016) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2016, format = "markdown"), "results/top_icd-10_tables/2016.md")
+
+icd2017 = icd_counts %>% filter(year==2017) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2017, format = "markdown"), "results/top_icd-10_tables/2017.md")
+
+icd2018 = icd_counts %>% filter(year==2018) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2018, format = "markdown"), "results/top_icd-10_tables/2018.md")
+
+icd2019 = icd_counts %>% filter(year==2019) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2019, format = "markdown"), "results/top_icd-10_tables/2019.md")
+
+icd2020 = icd_counts %>% filter(year==2020) %>% arrange(desc(n)) %>% head(20)
+    write_lines(knitr::kable(icd2020, format = "markdown"), "results/top_icd-10_tables/2020.md")
